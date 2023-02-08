9cd2f230ef7175b856db9bb6b6dbb8d8cfa3ee62
andreseptian <andreseptian07@icloud.com>
2023-02-08 14:32:24 +0700

    Implementasi Array

--- src/main/java/org/itc/exercise07/SelectionShort.java
+++ src/main/java/org/itc/exercise07/SelectionShort.java
@@ -3,7 +3,7 @@ package org.itc.exercise07;
 public class SelectionShort {
     public static void main(String[] args) {
         int[] list = { 5, 4, 8, 2, 9, 7, 1, 6, 3 };
-
+        int m = 1;
         System.out.println("List sebelum di urutkan ");
         for (int i = 0; i < list.length -1; i++) {
             System.out.print(list[i] + " ");
@@ -16,14 +16,15 @@ public class SelectionShort {
             int currentMinIndex = i;
 
             for (int j = i + 1; j < list.length; j++) {
-                System.out.println("putaran dalam ke " + j + " CurrentMin = " + currentMin + " > " + list[j]);
+                System.out.println("putaran dalam ke " + m + " CurrentMin = " + currentMin + " > " + list[j]);
 
                 if (currentMin > list[j]) {
                     currentMin = list[j];
                     currentMinIndex = j;
                 }
+                m++;
             }
-
+            m = 1;
             if (currentMinIndex != i) {
                 list[currentMinIndex] = list[i];
                 list[i] = currentMin;
@@ -36,6 +37,7 @@ public class SelectionShort {
                 System.out.print(" " + list[k]);
             }
             System.out.println(" ");
+
 
         }
 
